e40a690573652215442c21dcda422a022079708c
MikeG <iamgrudapps@gmail.com>
2023-04-24 10:51:50 +0200

    + Fixed tests

--- src/test/java/com/mgrud/github/proxy/gitproxycore/domain/integration/GithubProxyResourceIntegrationTest.java
+++ src/test/java/com/mgrud/github/proxy/gitproxycore/domain/integration/GithubProxyResourceIntegrationTest.java
@@ -45,18 +45,6 @@ public class GithubProxyResourceIntegrationTest {
 
 
     @Test
-    public void testExistingUserWithRepositoryWithoutBranches()
-            throws Exception {
-
-        mvc.perform(get("/user/repositories")
-                        .param("userName", "Aratek666")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[1].branches", empty()));
-    }
-
-    @Test
     public void testExistingUserWithoutAnyRepository()
             throws Exception {
 
@@ -70,15 +58,20 @@ public class GithubProxyResourceIntegrationTest {
     @Test
     public void testExistingUserRepositoryWithBranches()
             throws Exception {
-
         mvc.perform(get("/user/repositories")
                         .param("userName", "Aratek666")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].name", is("GtihubProxyTestRepo")))
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].name", is("GithubProxyAutorun")))
                 .andExpect(jsonPath("$[0].ownerLogin", is("Aratek666")))
-                .andExpect(jsonPath("$[0].branches[0].name", is("main")))
-                .andExpect(jsonPath("$[0].branches[0].sha", is("38a6a3b16e5d1bf302f2b39c32bf7af194e05332")));
+                .andExpect(jsonPath("$[0].branches[0].name", is("master")))
+                .andExpect(jsonPath("$[0].branches[0].sha", is("bad726a6863037f9ec6c6d67b8acb70cfb3dc58c")))
+                .andExpect(jsonPath("$[1].name", is("GtihubProxyTestRepo")))
+                .andExpect(jsonPath("$[1].ownerLogin", is("Aratek666")))
+                .andExpect(jsonPath("$[1].branches[0].name", is("main")))
+                .andExpect(jsonPath("$[1].branches[0].sha", is("38a6a3b16e5d1bf302f2b39c32bf7af194e05332")))
+                .andExpect(jsonPath("$[2].branches", empty()));
+
     }
 }
